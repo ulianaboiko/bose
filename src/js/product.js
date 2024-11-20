cc7decb713fb1283947f103375fb462cd889657f
unknown <bieldanil240@gamil.com>
2024-11-20 16:13:17 +0300

    .

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,25 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const image = document.getElementById('image');
+// const button1 = document.getElementById('button1');
+// const button2 = document.getElementById('button2');
+// const image = document.getElementById('image');
 
-button1.addEventListener('click', function () {
-  image.src = '../images/products/headphones-black.jpg';
-});
+// button1.addEventListener('click', function () {
+//   image.src = './images/products/headphones-black.jpg';
+// });
 
-button2.addEventListener('click', function () {
-  image.src = '../images/products/headphones-white.jpg';
+// button2.addEventListener('click', function () {
+//   image.src = './images/products/headphones-white.jpg';
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const button1 = document.getElementById('button1');
+  const button2 = document.getElementById('button2');
+  const image = document.getElementById('image');
+
+  button1.addEventListener('click', function () {
+    image.src = './images/products/headphones-black.jpg';
+  });
+
+  button2.addEventListener('click', function () {
+    image.src = './images/products/headphones-white.jpg';
+  });
 });
