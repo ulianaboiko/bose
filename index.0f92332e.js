85217d95cc224aa65b3a6dee70b7d07895959136
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2024-11-22 22:33:29 +0000

    Deploying to gh-pages from @ ulianaboiko/bose@e59242d045d94211f1d9b034c0839dbc1c57ed3b 🚀

---
+++ index.0f92332e.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("button1"),t=document.getElementById("button2"),n=document.getElementById("image");e.addEventListener("click",(function(){n.src="../images/products/headphones-black.jpg"})),t.addEventListener("click",(function(){n.src="../images/products/headphones-white.jpg"}));
+//# sourceMappingURL=index.0f92332e.js.map
---
+++ index.599fdf4f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("button1"),t=document.getElementById("button2"),n=document.getElementById("image");e.addEventListener("click",(function(){n.src="../images/products/headphones-black.jpg"})),t.addEventListener("click",(function(){n.src="../images/products/headphones-white.jpg"}))}();
+//# sourceMappingURL=index.599fdf4f.js.map
