14b627cea29074e6c3d5cca8118fcd4194fad684
iwan20131202 <harcenkovana93@gmail.com>
2024-12-03 19:45:04 +0300

    добавив файли

---
+++ src/js/slider.js
@@ -0,0 +1,18 @@
+const controls = document.querySelector('[data-pagination]');
+const slider = document.querySelector('[data-slider]');
+
+const handleSlider = event => {
+  const target = event.target;
+
+  if (target.tagName === 'BUTTON') {
+    console.log("click")
+    const index = target.dataset.slide;
+    slider.style.transform = `translateX(-${index}00%)`;
+
+    document.querySelectorAll('[data-slide]').forEach(btn => {
+      btn.classList.remove('active');
+    });
+    target.classList.add('active');
+  }
+};
+controls.addEventListener('click', handleSlider);
