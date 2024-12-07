282ad34eb42942ef0a1b3b58aa3f36ae443f20ec
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2024-12-07 19:23:55 +0000

    Deploying to gh-pages from @ ulianaboiko/bose@22b2325f1ce236efea4b0c1835ddc8eb54919b6d 🚀

--- index.bb456471.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){const e=document.getElementById("button1"),t=document.getElementById("button2"),n=document.getElementById("image");e.addEventListener("click",(function(){n.src="./images/products/headphones-black.jpg"})),t.addEventListener("click",(function(){n.src="./images/products/headphones-white.jpg"}))}));
-//# sourceMappingURL=index.bb456471.js.map
