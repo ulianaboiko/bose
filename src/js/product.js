a6be58f77afdaa2fe128b0ea7cb104dcda305919
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2024-12-10 20:55:21 +0200

    fixed slider in products

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,17 @@
-const controls = document.querySelector('[data-pagination]');
-const slider = document.querySelector('[data-slider]');
+const controls = document.querySelector('[data-pagin]');
+const slides = document.querySelector('[data-slides]');
 
-const handleSlider = event => {
+const handleSlides = event => {
   const target = event.target;
 
   if (target.tagName === 'BUTTON') {
-    const index = target.dataset.slide;
-    slider.style.transform = `translateX(-${index}00%)`;
+    const index = target.dataset.slid;
+    slides.style.transform = `translateX(-${index}00%)`;
 
-    document.querySelectorAll('[data-slide]').forEach(btn => {
+    document.querySelectorAll('[data-slid]').forEach(btn => {
       btn.classList.remove('active');
     });
     target.classList.add('active');
   }
 };
-controls.addEventListener('click', handleSlider);
+controls.addEventListener('click', handleSlides);
